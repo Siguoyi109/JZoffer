bdeab677933e92bacf89cb3dfae4681eeeb97712
斯国一 <92983122+Siguoyi109@users.noreply.github.com>
2022-01-03 19:38:01 +0800

    JZ2 反转链表

---
+++ JZ2 反转链表.cpp
@@ -0,0 +1,24 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+        ListNode *pre = nullptr;
+        ListNode *cur = pHead;
+        ListNode *nex = nullptr; // 这里可以指向nullptr，循环里面要重新指向
+        while (cur) {
+            nex = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nex;
+        }
+        return pre;
+
+    }
+};
